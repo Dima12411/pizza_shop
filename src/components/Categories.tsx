7c522f5b57ed3b5a60245c7b6a95ca7e4cd7c8fd
Dima12411 <fixer40delta@gmail.com>
2022-06-07 22:42:24 +0300

    I made the selection of categories by click

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Categories = () => {
+
+    const [activeIndex, setActiveIndex] = useState<number>(0)
+
+    const category = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые']
+
+    const onClickCategory = (index: number) => {
+        setActiveIndex(index)
+    }
+
     return (
         <div className="categories">
             <ul>
-                <li className="active">Все</li>
-                <li>Мясные</li>
-                <li>Вегетарианская</li>
-                <li>Гриль</li>
-                <li>Острые</li>
-                <li>Закрытые</li>
+                {
+                    category.map((el, i) => {
+                        return (
+                            <li className={activeIndex === i ? 'active' : ''}
+                                onClick={() => onClickCategory(i)}
+                            >{el}</li>
+                        )
+                    })
+                }
             </ul>
         </div>
     );
