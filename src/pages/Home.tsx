6468d337220fdeceb351c80c0ac3ef41932a719b
Dima12411 <fixer40delta@gmail.com>
2022-06-09 16:15:13 +0300

    refactoring

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -16,10 +16,11 @@ const Home = () => {
                 setItems(res)
                 setIsLoading(false)
             })
+        window.scrollTo(0, 0)
     }, [])
 
     return (
-        <>
+        <div className="container">
             <div className="content__top">
                 <Categories/>
                 <Sort/>
@@ -32,7 +33,7 @@ const Home = () => {
                         : items.map((el, index) => <PizzaBlock key={index} pizza={el}/>)
                 }
             </div>
-        </>
+        </div>
     );
 };
 
