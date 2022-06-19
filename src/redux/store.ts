67eff77e98fc0b8a3c54dc54d4880e26eb8b490c
Dima12411 <fixer40delta@gmail.com>
2022-06-19 19:19:21 +0300

    create cartSlice

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,11 @@
 import {combineReducers, configureStore} from "@reduxjs/toolkit";
 import thunkMiddleware from 'redux-thunk'
 import {filterReducer} from "./slices/filterSlice";
+import {cartReducer} from "./slices/cartSlice";
 
 const rootReducer = combineReducers({
     filter: filterReducer,
+    cart: cartReducer,
 })
 
 export const store = configureStore({
